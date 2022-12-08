ebe11bcd181f809e6dcf57709fb6ba39bb7c4bab
Agamor336 <120061141+Agamor336@users.noreply.github.com>
2022-12-08 14:09:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // https://github.com/microsoft/Web-Dev-For-Beginners/tree/main/7-bank-project/api
 // ***************************************************************************
 
-cd /var/lib/jenkins/workspace/HelloWorld
+response.end("Hello World!");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
